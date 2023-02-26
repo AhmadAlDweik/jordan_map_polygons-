7ac73d3f53819c5f56f3473f4028389e57dea9c4
ahmad dwaik <80591271+AhmadAlDweik@users.noreply.github.com>
2023-02-26 08:54:24 +0300

    .

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\fluttre\flutter_polyline_points\example"
+export "FLUTTER_APPLICATION_PATH=D:\fluttre\jordan_map_polygons-\example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
