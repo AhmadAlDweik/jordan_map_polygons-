5dc8490edc0d0e243a65346d50f402d0da9ddc08
ahmad dwaik <80591271+AhmadAlDweik@users.noreply.github.com>
2023-02-27 10:08:56 +0300

    edit

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,132 @@
-import 'dart:math';
-import 'dart:ui';
+// import 'package:flutter/material.dart';
+// import 'package:google_maps_flutter/google_maps_flutter.dart';
+//
+// void main() => runApp(MyApp());
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'My App',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//         visualDensity: VisualDensity.adaptivePlatformDensity,
+//       ),
+//       home: MapScreen(),
+//     );
+//   }
+// }
+//
+// class MapScreen extends StatefulWidget {
+//   @override
+//   _MapScreenState createState() => _MapScreenState();
+// }
+//
+// class _MapScreenState extends State<MapScreen> {
+//   GoogleMapController mapController;
+//   Map<MarkerId, Marker> markers = {};
+//   Map<PolygonId, Polygon> polygons = {};
+//
+//   // @override
+//   // void initState() {
+//   //   super.initState();
+//   //
+//   //   // Create a polygon and add it to the `polygons` map
+//   //   List<LatLng> polygonPoints = [
+//   //     LatLng(37.785972, -122.405506),
+//   //     LatLng(37.786364, -122.407557),
+//   //     LatLng(37.785092, -122.408569),
+//   //     LatLng(37.784796, -122.406448),
+//   //   ];
+//   //   Color colors = Colors.red.withOpacity(0.5);
+//   //
+//   //   PolygonId polygonId = PolygonId('my_polygon');
+//   //   Polygon polygon = Polygon(
+//   //     polygonId: polygonId,
+//   //     points: polygonPoints,
+//   //     strokeWidth: 2,
+//   //     strokeColor: Colors.red,
+//   //     fillColor: colors,
+//   //     consumeTapEvents: true,
+//   //     onTap: () {
+//   //       // This is called when the polygon is tapped
+//   //       // String polygonId = polygon.polygonId.value;
+//   //       print('Polygon tapped: ${polygonId.value}');
+//   //       setState(() {
+//   //         colors= Colors.blue.withOpacity(0.5);
+//   //         print('Polygon tapped: $colors');
+//   //
+//   //       });
+//   //
+//   //     },
+//   //   );
+//   //
+//   //   polygons[polygonId] = polygon;
+//   // }
+//
+//   void initState() {
+//     super.initState();
+//
+//     // Define some polygon points
+//     List<LatLng> polygon1Points = [
+//       LatLng(37.784902, -122.412652),
+//       LatLng(37.784801, -122.410881),
+//       LatLng(37.783873, -122.410903),
+//       LatLng(37.783978, -122.412673),
+//     ];
+//
+//     List<LatLng> polygon2Points = [
+//       LatLng(37.784675, -122.408963),
+//       LatLng(37.784515, -122.407922),
+//       LatLng(37.783772, -122.408144),
+//       LatLng(37.783956, -122.409174),
+//     ];
+//
+//     // Add the polygons to the `polygons` map
+//     polygons[PolygonId('polygon_1')] = Polygon(
+//       polygonId: PolygonId('polygon_1'),
+//       points: polygon1Points,
+//       strokeWidth: 2,
+//       strokeColor: Colors.red,
+//       fillColor: Colors.red.withOpacity(0.5),
+//       consumeTapEvents: true,
+//       onTap: () => print('Polygon 1 tapped!'),
+//     );
+//
+//     polygons[PolygonId('polygon_2')] = Polygon(
+//       polygonId: PolygonId('polygon_2'),
+//       points: polygon2Points,
+//       strokeWidth: 2,
+//       strokeColor: Colors.blue,
+//       fillColor: Colors.blue.withOpacity(0.5),
+//       consumeTapEvents: true,
+//       onTap: () => print('Polygon 2 tapped!'),
+//     );
+//   }
+//
+//   void _onMapCreated(GoogleMapController controller) {
+//     mapController = controller;
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Map Screen'),
+//       ),
+//       body: GoogleMap(
+//         onMapCreated: _onMapCreated,
+//         initialCameraPosition: CameraPosition(
+//           target: LatLng(37.785972, -122.405506),
+//           zoom: 15,
+//         ),
+//         polygons: Set<Polygon>.of(polygons.values),
+//       ),
+//     );
+//   }
+// }
+///todo
+// import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -9,7 +136,6 @@ import 'package:http/http.dart' as http;
 // import 'package:maps_toolkit/maps_toolkit.dart' as map;z`
 
 void main() => runApp(const MyApp());
-
 
 class MyApp extends StatefulWidget {
   const MyApp({key});
@@ -62,7 +188,8 @@ class _MyAppState extends State<MyApp> {
 }
 
 class MapScreen extends StatefulWidget {
-  MapScreen(this.currentMapType, {
+  MapScreen(
+    this.currentMapType, {
     Key key,
   }) : super(key: key);
 
@@ -78,6 +205,7 @@ class _MapScreenState extends State<MapScreen> {
   _MapScreenState(MapType currentMapType);
 
   // String customMapStyle2;
+  Color colors = Color.fromRGBO(0, 0, 255, 0.4117647058823529);
 
   @override
   void initState() {
@@ -87,63 +215,71 @@ class _MapScreenState extends State<MapScreen> {
     super.initState();
   }
 
-
   @override
   Widget build(BuildContext context) {
     return GetBuilder<MapConfigure>(
       init: MapConfigure(),
-      builder: (mapConfigure) =>
-          GoogleMap(
-            markers: mapConfigure.markersGroup,
-            initialCameraPosition: CameraPosition(target: _center, zoom: 7.5),
-            mapType: widget.currentMapType,
-            // mapType:mapConfigure.mapType? MapType.normal:MapType.satellite,
-            zoomGesturesEnabled: true,
-            zoomControlsEnabled: true,
-            compassEnabled: false,
-            mapToolbarEnabled: false,
-            myLocationButtonEnabled: true,
-            myLocationEnabled: true,
-            indoorViewEnabled: true,
-            trafficEnabled: true,
-            onMapCreated: (GoogleMapController controller) async {
-              String customMapStyle2 = await rootBundle.loadString(
-                  'assets/map_style.json');
+      builder: (mapConfigure) => GoogleMap(
+        markers: mapConfigure.markersGroup,
+        initialCameraPosition: CameraPosition(target: _center, zoom: 7.5),
+        mapType: widget.currentMapType,
+        // mapType:mapConfigure.mapType? MapType.normal:MapType.satellite,
+        zoomGesturesEnabled: true,
+        zoomControlsEnabled: true,
+        compassEnabled: false,
+        mapToolbarEnabled: false,
+        myLocationButtonEnabled: true,
+        myLocationEnabled: true,
+        indoorViewEnabled: true,
+        trafficEnabled: true,
+        onMapCreated: (GoogleMapController controller) async {
+          String customMapStyle2 =
+              await rootBundle.loadString('assets/map_style.json');
 
-              controller.setMapStyle(customMapStyle2);
-              await mapConfigure.getPointFromJison(context);
-            },
-            onTap: (LatLng latLng) async {
-              print("polygons ${PointMode.polygon.name}");
-              ///todo  setMarkerOnTapped
-              // await mapConfigure.setMarkerOnTapped(latLng);
-              await mapConfigure.setMarkerOnTappedWithPolygon(latLng);
-            },
-            polygons: {
-              ...List.generate(
-                  mapConfigure.polygonCoords.length,
-                      (index) =>
-                      Polygon(
-                        polygonId: PolygonId("$index"),
-                        points: mapConfigure.polygonCoords[index],
-                        fillColor:
-
-                        // Color.fromRGBO(
-                        //     66, 75, 245, 0.7294117647058823),
-                        index != mapConfigure.polygonCoords.length-1
-                            ?
-                        Color.fromARGB(
-                          Random().nextInt(256),
-                          Random().nextInt(256),
-                          Random().nextInt(256),
-                          150,
-                        )
-                            : Colors.white10,
-                        strokeWidth: 4,
-                      )),
-            },
-
-          ),
+          controller.setMapStyle(customMapStyle2);
+          await mapConfigure.getPointFromJison(context);
+        },
+        onTap: (LatLng latLng) async {
+          // print("polygons ${PointMode.polygon.name}");
+          // print("polygons ${PointMode.points.index}");
+          // print("polygons ${PointMode.values}");
+          ///todo  setMarkerOnTapped
+          // await mapConfigure.setMarkerOnTapped(latLng);
+          // await mapConfigure.setMarkerOnTappedWithPolygon(latLng);
+        },
+        polygons: {
+          ...List.generate(
+              mapConfigure.polygonCoords.length,
+              (index) => Polygon(
+                    polygonId: PolygonId("$index"),
+                    points: mapConfigure.polygonCoords[index],
+                    fillColor: mapConfigure.index==index?Color.fromRGBO(0, 255, 0, 0.4117647058823529):colors,
+                    // index != mapConfigure.polygonCoords.length-1
+                    //     ?
+                    // Color.fromARGB(
+                    //   Random().nextInt(256),
+                    //   Random().nextInt(256),
+                    //   Random().nextInt(256),
+                    //   150,
+                    // )
+                    //     : Colors.white10,
+                    strokeWidth: 4,
+                    consumeTapEvents: true,
+                    geodesic: true,
+                    onTap: () {
+                      print('Polygon $index tapped!');
+                      mapConfigure.index =index;
+                      setState(() {
+                        colors = (colors == Color.fromRGBO(0, 0, 255, 0.4117647058823529))
+                            ? Color.fromRGBO(0, 0, 255, 0.4117647058823529)
+                            : Color.fromRGBO(0, 0, 255, 0.4117647058823529);
+                      });
+                    },
+                    // onTap: () {
+                    // },
+                  )),
+        },
+      ),
     );
   }
 }
@@ -158,6 +294,7 @@ class MapConfigure extends GetxController {
 
   bool mapType = true;
 
+  int index =-1;
 
   changeMapType(bool type) {
     mapType = type;
@@ -175,8 +312,8 @@ class MapConfigure extends GetxController {
         'https://api.geoapify.com/v1/boundaries/part-of?lon=$lon&lat=$lat&geometry=geometry_1000&apiKey=e15ee377d78e4a259ecf62b9888463d6'));
     try {
       if (
-      // response.statusCode == 200 &&
-      response2.statusCode == 200) {
+          // response.statusCode == 200 &&
+          response2.statusCode == 200) {
         // featuresCount = json.decode(response.body)['features'];
         featuresCount2 = json.decode(response2.body)['features'];
         // for (int i = 0; i < featuresCount.length-1; i++) {
@@ -189,7 +326,7 @@ class MapConfigure extends GetxController {
         for (int i = 0; i < featuresCount2.length; i++) {
           polygonCoords.add(json
               .decode(response2.body)['features'][i]['geometry']['coordinates']
-          [0]
+                  [0]
               .map<LatLng>((coord) => LatLng(coord[1], coord[0]))
               .toList());
         }
@@ -222,7 +359,6 @@ class MapConfigure extends GetxController {
   }
 
   setMarkerOnTapped(LatLng latLng) async {
-
     markersGroup.clear();
     marker1 = (Marker(
       markerId: MarkerId("1"),
@@ -235,36 +371,57 @@ class MapConfigure extends GetxController {
     update();
   }
 
-
   Future getPointFromJison(context) async {
     polygonCoords = [];
-    var response = await DefaultAssetBundle.of(context).loadString(
-        'assets/Untitled_layer_test2.geojson');
+    var response = await DefaultAssetBundle.of(context)
+        .loadString('assets/Untitled_layer_test2.geojson');
     print("response getPointFromJison $response");
     var data = json.decode(response);
     List data2 = json.decode(response)['features'];
     print("data2 ${data2.length}");
-    for (int i = 0; i < data2.length; i++) {
-      polygonCoords.add(data['features'][i]['geometry']['coordinates']
-      [0]
+    for (int i = 0; i < data2.length-1; i++) {
+      polygonCoords.add(data['features'][i]['geometry']['coordinates'][0]
           .map<LatLng>((coord) => LatLng(coord[1], coord[0]))
           .toList());
     }
     print("polygonCoords $polygonCoords");
+    print("polygonCoords ${polygonCoords.length}");
+
+    // List.generate(
+    //     polygonCoords.length,
+    //         (index) =>
+    //     polygons[PolygonId('polygon_$index')] =  Polygon(
+    //       polygonId: PolygonId('polygon_$index'),
+    //       points: polygonCoords[index],
+    //       fillColor:
+    //
+    //       // Color.fromRGBO(
+    //       //     66, 75, 245, 0.7294117647058823),
+    //       index != polygonCoords.length-1
+    //           ?
+    //       Color.fromARGB(
+    //         Random().nextInt(256),
+    //         Random().nextInt(256),
+    //         Random().nextInt(256),
+    //         150,
+    //       )
+    //           : Colors.white10,
+    //       strokeWidth: 4,
+    //       onTap: () => print('Polygon $index tapped!'),
+    //
+    //     ));
     update();
   }
 
   Set<Polygon> _polygons = {};
 
-
-
   void _onPolygonTapped(Polygon polygon) {
-        _polygons = _polygons.map((Polygon p) {
-        if (p.polygonId == polygon.polygonId) {
-          return p.copyWith(fillColorParam: Colors.green.withOpacity(0.5));
-        }
-        return p.copyWith(fillColorParam: Colors.transparent);
-      }).toSet();
+    _polygons = _polygons.map((Polygon p) {
+      if (p.polygonId == polygon.polygonId) {
+        return p.copyWith(fillColorParam: Colors.green.withOpacity(0.5));
+      }
+      return p.copyWith(fillColorParam: Colors.transparent);
+    }).toSet();
     update();
   }
 
